8cc3036f25d9f2226c0c24a8ec8d735264d7320b
Manish <manish123@gmail.com>
2022-06-28 18:33:25 +0530

    added login api

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import "./LoginForm.css";
+// import axios from "axios";
 
 function LoginForm() {
-  const [email, setemail] = useState("");
+  const [userName, setuserName] = useState("");
   const [password, setPassword] = useState("");
-  const [msg,setmsg] = useState("");
-  const emailHandler = (e) => {
-    setemail(e.target.value);
+  // const [msg, setmsg] = useState("");
+  const userNameHandler = (e) => {
+    setuserName(e.target.value);
   };
 
   const passwordHandler = (e) => {
@@ -16,70 +17,89 @@ function LoginForm() {
   //handles submit button click
   const loginHandler = async (e) => {
     e.preventDefault();
-    if (email === "" || password === "") {
-      // alert("Please enter email and password");
-      if (email !== "" && password === "")
-       {
+    if (userName === "" || password === "") {
+      // alert("Please enter userName and password");
+      if (userName !== "" && password === "") {
         alert("Please Enter Password");
         console.log("password is empty");
-      }
-
-       else if (email === "" && password !== "")
-        {
-
-        alert("Please Enter email");
-        console.log("email is empty");
-
-
-      }
-       else
-        {
-        alert("Please Enter email and Password");
+      } else if (userName === "" && password !== "") {
+        alert("Please Enter userName");
+        console.log("userName is empty");
+      } else {
+        alert("Please Enter userName and Password");
         console.log("both inputs are empty");
-
       }
-    }
-    else{
-        // console.log(email);
+    } else {
+      
+       
+
+        // console.log(userName);
         // console.log(password);
-        alert("Login successful");
-        setemail("");
+        // alert("Login successful");
+        setuserName("");
         setPassword("");
-        setmsg("Login Successful");
+        // setmsg("Login Successful");
+        // fake api "http://restapi.adequateshop.com/api/authaccount/login" "http://192.168.3.170/VisitorMangaementSystems/Auth/Login
+        // let item = { userName, password };
+        // console.log(item);
+        // await fetch(
+        //   `http://192.168.3.170/VisitorMangaementSystems/Auth/Login?userName=${userName}&password=${password}`,
+        //   {
+        //     method: "POST",
+        //     // mode: "no-cors",
+        //     headers: {
+        //      'Content-Type':'application/x-www-form-urlencoded',
+        //      "Access-Control-Allow-Origin":'*',
+        //      "Access-Control-Allow-Credentials" : true
+        //     },
+        //     // body:JSON.stringify(item),
+        //   }
+        // )
+        //   .then((response) =>{
+        //     console.log(response);
+        //     console.log(typeof(response));}
+        //   )
+        //   .then(result => {
+        //     console.log("result:::",result)
+        //     JSON.parse(result);
+        //   });
 
-        let item = {email,password};
-         await fetch("http://restapi.adequateshop.com/api/authaccount/login",{
-          method:'POST',
-          headers:{
-            "Content-Type": "application/json",
-            "Accept": 'application/json'
-          },
-          body:JSON.stringify(item)
-        }).then(response => 
-          console.log(response)
-        );
         // result = await result.json();
-        // localStorage.setItem(JSON.stringify(result))
-    
+        // localStorage.setItem(JSON.stringify(result)
 
-    }
+        var requestOptions = {
+          mode : 'no-cors',
+          method: 'POST',
+          redirect: 'follow',
+         
+        };
+        
+        fetch(`http://192.168.3.170/VisitorMangaementSystems/Auth/Login?userName=${userName}&password=${password}`, requestOptions)
+          .then(response =>{ response.text(); console.log("response:::",response)})
+          .then(result => console.log("result :::: ",typeof(result)))
+          .catch(error => console.log('error:::::', error));
+        
+      } 
+      
+      
+    
   };
+
   return (
     <>
-      <form>
+      <form method="POST">
         <div className="mainForm">
           <h2>Login</h2>
 
           <div className="textBox">
-            <label htmlFor="user name">email</label>
+            <label htmlFor="user name">userName</label>
             <br />
             <input
               type="text"
               className="inputBox"
-              value={email}
-              onChange={emailHandler}
+              value={userName}
+              onChange={userNameHandler}
               autoFocus
-              
             />
             <br />
             <label htmlFor="password">password</label>
@@ -95,7 +115,9 @@ function LoginForm() {
             LOGIN
           </button>
         </div>
-        <h1 style={{color:'Green',textAlign:'center',marginTop:'10px'}}>{msg}</h1>
+        <h1 style={{ color: "Green", textAlign: "center", marginTop: "10px" }}>
+          {/* {msg} */}
+        </h1>
       </form>
     </>
   );
